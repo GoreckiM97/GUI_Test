68a9e4c90436343630c588bb6c72c7df4e107261
wnsah12 <kwonj@ripon.edu>
2020-02-26 08:11:33 -0600

    GUIButton

---
+++ Assets/Click Button.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickButton : MonoBehaviour
+{
+    public void Clicked(
+        Debug.Log("You have clicked the button!!!");
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
